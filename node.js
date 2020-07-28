0fe40f8036ac956ea2ae4bd4e1103486bc202e6f
gonzalopla <gonzalo_p@csu.fullerton.edu>
2020-07-27 20:36:23 -0700

    works bois

--- node.js
+++ node.js
@@ -14,62 +14,86 @@ var input=document.getElementById('inputid');
   //add filter otptions here then append to end of query with "+"
 //looking for way to add filter through checkbox
 //right now all filters are activated
-//var stringsum=null;
+var stringsum=undefined;
+var final="";
+var stringsum1=undefined;
+var stringsum2=undefined;
+var stringsum3=undefined;
+var stringsum4=undefined;
+var stringsum5=undefined;
+var stringsum6=undefined;
+var stringsum7=undefined;
+var stringsum8=undefined;
+var stringsum9=undefined;
+var stringsum10=undefined;
 var filtert=document.getElementById('filter1')
 if (filtert.checked) {
- var stringsum1 = document.getElementById('filter1');
- //stringsum=[stringsum, stringsum1.value].filter(function(val){return val!==null;}).join('');
+   stringsum1 = document.getElementById('filter1');
+   stringsum=1;
 }
-/*var filtert=document.getElementById('filter2')
+var filtert=document.getElementById('filter2')
 if (filtert.checked) {
- var stringsum2= document.getElementById('filter2');
- stringsum=[stringsum, stringsum2.value].filter(function(val){return val!==null;}).join('');
+  stringsum2= document.getElementById('filter2');
+  stringsum=1;
 }
 var filtert=document.getElementById('filter3')
 if (filtert.checked) {
- var stringsum3= document.getElementById('filter3');
- stringsum=[stringsum, stringsum3.value].filter(function(val){return val!==null;}).join('');
+  stringsum3= document.getElementById('filter3');
+  stringsum=1;
 }
 var filtert=document.getElementById('filter4')
 if (filtert.checked) {
- var stringsum4 = document.getElementById('filter4');
- stringsum=[stringsum, stringsum4.value].filter(function(val){return val!==null;}).join('');
+  stringsum4 = document.getElementById('filter4');
+  stringsum=1;
 }
 var filtert=document.getElementById('filter5')
 if (filtert.checked) {
- var stringsum5 = document.getElementById('filter5');
- stringsum=[stringsum, stringsum5.value].filter(function(val){return val!==null;}).join('');
+  stringsum5 = document.getElementById('filter5');
+  stringsum=1;
 }
 var filtert=document.getElementById('filter6')
 if (filtert.checked) {
- var stringsum6= document.getElementById('filter6');
- stringsum=[stringsum, stringsum6.value].filter(function(val){return val!==null;}).join('');
+  stringsum6= document.getElementById('filter6');
+  stringsum=1;
 }
 var filtert=document.getElementById('filter7')
 if (filtert.checked) {
- var stringsum7= document.getElementById('filter7');
- stringsum=[stringsum, stringsum7.value].filter(function(val){return val!==null;}).join('');
+  stringsum7= document.getElementById('filter7');
+  stringsum=1;
 }
 var filtert=document.getElementById('filter8')
 if (filtert.checked) {
- var stringsum8= document.getElementById('filter8');
- stringsum=[stringsum, stringsum8.value].filter(function(val){return val!==null;}).join('');
+  stringsum8= document.getElementById('filter8');
+  stringsum=1;
 }
 var filtert=document.getElementById('filter9')
 if (filtert.checked) {
- var stringsum9= document.getElementById('filter9');
- stringsum=[stringsum, stringsum9.value].filter(function(val){return val!==null;}).join('');
+  stringsum9= document.getElementById('filter9');
+  stringsum=1;
 }
 var filtert=document.getElementById('filter10')
 if (filtert.checked) {
- var stringsum10= document.getElementById('filter10');
- stringsum=[stringsum, stringsum10.value].filter(function(val){return val!==null;}).join('');
-}*/
+  stringsum10= document.getElementById('filter10');
+  stringsum=1;
+}
 //stringsum=[stringsum1.value,stringsum2.value,stringsum3.value,stringsum4.value,stringsum5.value,stringsum6.value,stringsum7.value,stringsum8.value,stringsum9.value,stringsum10.value].join('');
+//var stringsum2='';
+var data = [stringsum1, stringsum2, stringsum3, stringsum4, stringsum5, stringsum6, stringsum7, stringsum8, stringsum9, stringsum10];
+
+data = data.filter(function( element ) {
+   return element !== undefined;
+});
+for(var i=0; i<data.length;i++){
+   final+=data[i].value;
+}
+console.log(final);
+console.log(stringsum);
+
+
   let APP_ID="2afa29fc";
   let API_KEY="3063fe6d505174b0306c307e4268fe78";
-if(stringsum1!==undefined){
-  let response = await fetch(`https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=`+input.value+stringsum1.value);
+if(stringsum!==undefined){
+  let response = await fetch(`https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=`+input.value+final);
     let data=await response.json()
     console.log(response)
     useApiData(data)
